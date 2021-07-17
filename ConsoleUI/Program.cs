824f18e2455035adbec4f6f374e83f453e39d43e
Çağrı Türkmen <cagritrkmn33@gmail.com>
2021-07-17 19:24:59 +0300

    10.gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,10 +10,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductTest();
+            //ProductTest();
             //CategoryGetByIdTest();
 
-
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetAll();
+            if (result.Success == true)
+            {
+                foreach (var product in productManager.GetAll().Data)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
 
         private static void CategoryGetByIdTest()
@@ -26,11 +39,20 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if(result.Success== true)
             {
-                Console.WriteLine(product.ProductName+"/"+ product.CategoryName);
+                   foreach (var product in productManager.GetProductDetails().Data)
+                   {
+                       Console.WriteLine(product.ProductName+"/"+ product.CategoryName);
+                   }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
         }
     }
 }
