e87f7fabfb50fbaf589432eba0d405189416d1e0
Çağrı Türkmen <cagritrkmn33@gmail.com>
2021-08-04 16:29:47 +0300

    14.gün 2.commit

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper// bu klasın amacı appsettings.json da security key i string olarak oluyşturduğumuz yapıy byte[] yapısına çevirdik(diğer uyg anlasın diye )
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;// burada saltlamamız içşn keyi seçtik her kullanıcıya farklı verir yani uniquedir  
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+        public static bool VerifyPasswordHash(string password,  byte[] passwordHash,  byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))// bu hmacsha512 classı bizden bir key istiyor o da bibzim yukarda passwordsaltımız
+            {
+               var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            
+        }
+    }
+}
